9e5c9b68ca3b75d6a8aa0a010e6a24f1341c5222
Sadegh Dehghani <dehghani.prg@outlook.com>
2025-09-26 11:23:18 +0330

    PdfJsUpdate

--- HtmlToPdf/Controllers/HomeController.cs
+++ HtmlToPdf/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using HtmlToPdf.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,15 +7,43 @@ namespace HtmlToPdf.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger)
+
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            // خواندن لیست فایل‌های HTML از پوشه wwwroot/html
+            var folderPath = Path.Combine(_env.WebRootPath, "html");
+            var files = Directory.GetFiles(folderPath, "*.html")
+                                 .Select(Path.GetFileName)
+                                 .ToList();
+
+            return View(files);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetHtmlFile(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) return NotFound();
+
+            var filePath = Path.Combine(_env.WebRootPath, "html", fileName);
+            if (!System.IO.File.Exists(filePath)) return NotFound();
+
+            var content = System.IO.File.ReadAllText(filePath);
+            return Content(content, "text/html");
         }
 
         public IActionResult Privacy()
